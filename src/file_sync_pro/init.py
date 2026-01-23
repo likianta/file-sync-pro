5668d086a0e3515a83ed78cf826669a4ad424eb7
Likianta <likianta@foxmail.com>
2026-01-23 10:48:37 +0800

    fix `clone_snapshot`

--- src/file_sync_pro/init.py
+++ src/file_sync_pro/init.py
@@ -1,4 +1,3 @@
-from lk_utils import fs
 from .filesys import LocalFileSystem
 from .snapshot import Snapshot
 
@@ -8,9 +7,11 @@ def clone_project(
 ) -> None:
     """
     note:
-        1. make sure `<root_i>/snapshot.json` exists. if not, use -
-            `.main.create_snapshot()` to create one.
-        2. make sure `fs.parent(snapshot_file_o)` does not exist.
+        1. make sure `snapshot_file_i` is latest, if not, use
+            `./__main__.py:update_snapshot` to update.
+        2. make sure `snapshot_file_o` exists, if not, use
+            `./__main__.py:create_snapshot` to create one.
+        FIXME: `root_o` is not available at the moment.
     """
     snap_i = Snapshot(snapshot_file_i)
     snap_o = Snapshot(snapshot_file_o)
@@ -18,13 +19,13 @@ def clone_project(
     
     snap_full_i = snap_i.load_snapshot()
     root_i = snap_full_i['root']
-    root_o = root_o or fs.parent(snap_o.snapshot_file)
+    root_o = root_o or snap_o.source_root
     print('{} -> {}'.format(root_i, root_o))
     
     # make empty dirs
     # tobe_created_dirs = set()
     tobe_created_dirs = {root_o}
-    snap_data_i = snap_full_i['current']['data']
+    snap_data_i = snap_full_i['current']['files']
     for relpath in snap_data_i:
         if '/' in relpath:
             d = root_o
@@ -32,11 +33,12 @@ def clone_project(
                 d += '/' + x
                 tobe_created_dirs.add(d)
     for d in sorted(tobe_created_dirs):
+        print(':i2', 'make dir', d.removeprefix(root_o))
         snap_o.fs.make_dir(d)
     
     # copy files
     for relpath, mtime in snap_data_i.items():
-        print(':i', relpath)
+        print(':i2', 'copy file', relpath)
         file_i = f'{root_i}/{relpath}'
         file_o = f'{root_o}/{relpath}'
         snap_o.fs.upload_file(file_i, file_o, mtime)
